7ef6d3225411ec27aaa20184d366eec9f8b7e3ca
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-08-27 07:42:15 -0700

    minor cleanup of shiny_diurnalPattern()

--- app/shiny_diurnalPattern.R
+++ app/shiny_diurnalPattern.R
@@ -3,20 +3,29 @@
 shiny_diurnalPattern <-
   function(pat) {
 
-    if (TRUE) {
-      sensor <- example_sensor
-    }
-
     logger.debug('----- shiny_diurnalPattern() -----')
 
     sensor <- AirSensor::pat_createAirSensor(pat)
 
-   p1 <- AirMonitorPlots::ggplot_pm25Diurnal(ws_data = sensor, offsetBreaks = TRUE) +
-     scale_fill_sqamd()
+    p1 <-
+      AirMonitorPlots::ggplot_pm25Diurnal(
+        ws_data = sensor,
+        offsetBreaks = TRUE
+      ) +
+      stat_meanByHour() +
+      scale_fill_sqamd()
 
-   p2 <- AirMonitorPlots::monitor_ggClockPlot(sensor) + scale_fill_sqamd()
+    p2 <- AirMonitorPlots::monitor_ggClockPlot(sensor) + scale_fill_sqamd()
 
 
-   AirSensor::multi_ggplot(plotList = list(p1, p2),cols = 2)
+    AirSensor::multi_ggplot(plotList = list(p1, p2),cols = 2)
   }
+
+# ===== DEBUGGING ==============================================================
+
+if ( FALSE ) {
+
+  sensor <- example_sensor
+
+}
 
