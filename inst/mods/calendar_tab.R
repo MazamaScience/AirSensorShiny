82a03375f73c5d55a6df174fef0bcbd24aee761f
Hans Martin <hansmrtn@gmail.com>
2020-01-21 23:54:10 -0800

    added calendar tab refactor WIP

--- inst/mods/calendar_tab.R
+++ inst/mods/calendar_tab.R
@@ -1,3 +1,6 @@
+#' TAB: Calendar User Interface
+#'
+#' @param id
 calendar_tab_ui <- function(id) {
   ns <- shiny::NS(id)
   shiny::tagList(
@@ -8,6 +11,12 @@ calendar_tab_ui <- function(id) {
   )
 }
 
+#' TAB: Calendar Logic
+#'
+#' @param input
+#' @param output
+#' @param session
+#' @param active
 calendar_tab <- function(input, output, session, active) {
 
   output$calendar <- plotly::renderPlotly({
